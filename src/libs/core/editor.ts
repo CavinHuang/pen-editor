1c08c0f5222bbd63d511fd534e1ec97c71cdc855
Leo <huangchunmao1993@gmail.com>
2025-04-07 22:15:46 +0800

    增强 PenEditor 类，添加状态序列化和选择处理功能，优化事件处理和偏移设置逻辑，改进文本节点迭代和更新机制。

--- src/libs/core/editor.ts
+++ src/libs/core/editor.ts
@@ -1,5 +1,7 @@
 import { PenEditorOptions, PenEditorParser, PenEditorPlugin, PenEditorRenderer, PenEditorSelection } from '../typings';
-import { getTypeOffset } from '../utils/offset';
+import { getTypeOffset, setOffset } from '../utils/offset';
+import { serializeState } from '../utils/selection';
+
 
 export class PenEditor {
   element: HTMLElement;
@@ -37,17 +39,180 @@ export class PenEditor {
 
     this.plugins = plugins;
 
+    const _element = this.element;
     this.selection = {
       anchorBlock: 0,
       focusBlock: 0,
       get anchorOffset() {
-        return getTypeOffset('anchor', this.element);
+        return getTypeOffset('anchor', _element);
       },
       get focusOffset() {
-        return getTypeOffset('focus', this.element);
+        return getTypeOffset('focus', _element);
       }
     };
 
+    this.element.contentEditable = 'true';
+    changeHandlers(this, 'add');
+    this.value = value;
+  }
 
+
+  /**
+   * @private
+   */
+  handleEvent(event) {
+    callPlugins(this, ['handlers', event.type], event);
+  }
+
+  /**
+   * @param {StateNode[]} state
+   * @param {[Number, Number]|{ anchor: [Number, Number], focus: [Number, Number] }} caret
+   */
+  update(state, caret = [0, 0]) {
+    if (!caret.anchor) {
+      caret = { focus: caret, anchor: caret.slice() };
+    }
+
+    for (const plugin of this.plugins) {
+      const handler = plugin.beforeupdate;
+      if (!handler) continue;
+      const ret = handler(this, state, caret);
+      if (!ret) continue;
+      state = ret.state;
+      caret = ret.caret;
+    }
+
+    this.state = state;
+    setOffset(this, caret);
+  }
+
+   /**
+   * @param {StateNode[]} state
+   */
+   set state(state) {
+    if (state === this.state) return;
+
+    const prevState = this.state;
+    this._state = state;
+
+    state.forEach((node, index) => {
+      const current = this.element.children[index];
+
+      if (prevState.includes(node)) {
+        // Avoid having to recreate nodes that haven't changed
+        const prevIndex = prevState.indexOf(node);
+        const el = this._elements[prevIndex];
+
+        if (el === current) return;
+        this.element.insertBefore(el, current);
+      } else {
+        const el = toDOM(this.renderer, node);
+
+        // Improves caret behavior when contenteditable="false"
+        // is the last child or when empty
+        if (
+          !el.childNodes.length ||
+          (safari || firefox) &&
+          el.lastChild &&
+          el.lastChild.contentEditable === 'false'
+        ) {
+          el.append(document.createElement('br'));
+        }
+
+        const morph = !state.includes(prevState[index]);
+        if (morph && this._elements[index]) {
+          morphdom(this._elements[index], el);
+        } else {
+          this.element.insertBefore(el, current);
+        }
+      }
+    });
+
+    // Remove leftover elements
+    while (this.element.childElementCount > state.length) {
+      this.element.lastElementChild.remove();
+    }
+
+    this._elements = Array.from(this.element.children);
+
+    callPlugins(this, ['afterchange']);
+  }
+
+  /**
+   * @returns {StateNode[]}
+   */
+  get state() {
+    return this._state;
+  }
+
+  /**
+   * @param {String} value
+   */
+  set value(value) {
+    this.update(Array.from(this.parser(value)));
+  }
+
+  /**
+   * @returns {String}
+   */
+  get value() {
+    return serializeState(this.state, true);
+  }
+
+  destroy() {
+    changeHandlers(this, 'remove');
+  }
+}
+
+// ================================ Change Handlers ================================
+const EVENTS = [
+  'beforeinput',
+  'compositionstart',
+  'compositionend',
+  'copy',
+  'dragstart',
+  'drop',
+  'paste',
+  'input',
+  'keydown',
+  'keypress'
+];
+
+const DOCUMENT_EVENTS = [
+  'selectionchange'
+];
+
+
+/**
+ * @typedef {Object} StateNode
+ * @property {String} type
+ * @property {Array<StateNode|String>} content
+ */
+function changeHandlers(editor: PenEditor, cmd: 'add' | 'remove') {
+  for (const name of EVENTS) {
+    // TODO: Fix type
+    editor.element[`${cmd}EventListener`](name, editor);
+  }
+  for (const name of DOCUMENT_EVENTS) {
+    // TODO: Fix type
+    document[`${cmd}EventListener`](name, editor);
+  }
+}
+// TODO: Fix type
+function getPath(obj: any, path: string[]) {
+  for (const key of path) {
+    obj = obj[key];
+    if (!obj) return;
+  }
+  return obj;
+}
+
+/**
+ * Call plugins until one returns true
+ */
+function callPlugins(editor: PenEditor, path: string[], ...args: any[]) {
+  for (const plugin of editor.plugins) {
+    const handler = getPath(plugin, path);
+    if (handler && handler(editor, ...args)) break;
   }
 }
--- src/libs/utils/offset.ts
+++ src/libs/utils/offset.ts
@@ -7,13 +7,14 @@ export type PenEditorSelectionNode = 'anchorNode' | 'focusNode';
 export const getTypeOffset = (type: PenEditorSelectionType, element: HTMLElement) => {
   const sel = (element.getRootNode() as Document).getSelection();
   if (!sel) return -1;
-  const block = sel[type + 'Block' as PenEditorSelectionBlock];
+  // TODO: Fix type
+  const block = sel[type + 'Block'];
   if (sel[type + 'Node' as PenEditorSelectionNode] === element) return 0;
   if (!element.contains(sel[type + 'Node' as PenEditorSelectionNode])) return -1;
 
   return getOffset(
     element.children[block],
-    sel[type + 'Node' as PenEditorSelectionNode],
+    sel[type + 'Node' as PenEditorSelectionNode]!,
     sel[type + 'Offset' as PenEditorSelectionOffset]
   );
 };
@@ -26,23 +27,23 @@ export const getTypeOffset = (type: PenEditorSelectionType, element: HTMLElement
  * @param {Number} offset
  * @returns {Number}
  */
-export function getOffset(parent, target, offset) {
+export function getOffset(parent: Node, target: Node, offset: number) {
   // Start of line
   if (target === parent && offset === 0) return 0;
 
   if (target.nodeType !== Node.TEXT_NODE) {
     if (target === parent) {
       target = parent.childNodes[offset - 1];
-      if (target.tagName === 'BR') return 0;
+      if ((target as HTMLElement).tagName === 'BR') return 0;
 
       if (target.nodeType === Node.TEXT_NODE) {
-        offset = target.data.length;
-      } else if (target.dataset && 'text' in target.dataset) {
-        offset = target.dataset.text.length;
+        offset = (target as Text).data.length;
+      } else if ((target as HTMLElement).dataset && 'text' in (target as HTMLElement).dataset) {
+        offset = (target as HTMLElement).dataset.text?.length ?? 0;
       } else {
         const nodes = Array.from(iterateNodes(target));
-        target = nodes[nodes.length - 1].node;
-        offset = nodes[nodes.length - 1].text.length;
+        target = nodes[nodes.length - 1]?.node;
+        offset = nodes[nodes.length - 1]?.text?.length ?? 0;
       }
     } else {
       // Find nearest preceding node with text
@@ -56,7 +57,7 @@ export function getOffset(parent, target, offset) {
       }
       target = current;
       if (target === parent && offset === 0) return 0;
-      offset = target.dataset ? target.dataset.text.length : target.data.length;
+      offset = (target as HTMLElement).dataset?.text?.length ?? 0;
     }
   }
 
@@ -67,8 +68,118 @@ export function getOffset(parent, target, offset) {
       return pos + offset;
     }
 
-    pos += text.length;
+    pos += text?.length ?? 0;
   }
 
   return -1;
+}
+
+
+/**
+ * Replace non-breaking space with regular
+ */
+const NON_BREAKING_SPACE = new RegExp(String.fromCharCode(160), 'g');
+
+function normalizeText(text: string) {
+  return text.replace(NON_BREAKING_SPACE, ' ');
+}
+
+/**
+ * Create an Generator for all text nodes and elements with `data-text` attribute
+ */
+function* iterateNodes(parent: Node) {
+  const treeWalker = document.createTreeWalker(
+    parent,
+    NodeFilter.SHOW_TEXT + NodeFilter.SHOW_ELEMENT,
+    {
+      acceptNode(node) {
+        const accept = node.nodeType === Node.TEXT_NODE || (node as HTMLElement).dataset.text;
+        return accept ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP;
+      }
+    }
+  );
+
+  let node = treeWalker.nextNode();
+  while (node) {
+    if (node.nodeType === Node.ELEMENT_NODE) {
+      const text = (node as HTMLElement).dataset.text;
+      yield { node, text };
+      node = treeWalker.nextSibling();
+    } else {
+      const text = normalizeText((node as Text).data);
+      yield { node, text };
+      node = treeWalker.nextNode();
+    }
+  }
+}
+
+/**
+ * Get text of a block
+ */
+export function getText(node: Node) {
+  let text = '';
+
+  for (const val of iterateNodes(node)) {
+    text += val.text;
+  }
+
+  return text;
+}
+
+
+/**
+ * @param {Object} editor
+ * @param {[Number, Number]|{ anchor: [Number, Number], focus: [Number, Number] }} caret
+ */
+export function setOffset(editor, caret) {
+  const [anchorBlock, anchorOffset] = caret.anchor || caret;
+  const [focusBlock, focusOffset] = caret.focus || caret;
+
+  const startEl = editor.element.children[anchorBlock];
+  const endEl = editor.element.children[focusBlock];
+
+  const selection = editor.element.getRootNode().getSelection();
+  selection.removeAllRanges();
+  const range = document.createRange();
+
+  const anchorPosition = getOffsetPosition(startEl, anchorOffset);
+  range.setStart(anchorPosition.node, anchorPosition.offset);
+  selection.addRange(range);
+
+  if (anchorBlock !== focusBlock || anchorOffset !== focusOffset) {
+    const focusPosition = getOffsetPosition(endEl, focusOffset);
+    selection.extend(focusPosition.node, focusPosition.offset);
+  }
+}
+
+/**
+ * Find node and remaining offset for caret position
+ */
+export function getOffsetPosition(el: Node, offset: number) {
+  if (offset < 0) return { node: el, offset: 0 };
+
+  // eslint-disable-next-line prefer-const
+  for (let { node, text } of iterateNodes(el)) {
+    if (!text) continue;
+    if (text.length >= offset) {
+
+      if ((node as HTMLElement).dataset && 'text' in (node as HTMLElement).dataset) {
+        const prevOffset = offset;
+        offset = Array.from((node as HTMLElement).parentNode?.childNodes ?? []).indexOf(node as ChildNode);
+        if (prevOffset >= text.length) offset++;
+        node = node.parentNode!;
+      }
+
+      return { node, offset };
+    }
+
+    offset -= text.length;
+  }
+
+  if (offset > 0) {
+    // Continue to next block
+    return getOffsetPosition(el.nextSibling!, offset - 1);
+  }
+
+  return { node: el, offset: 0 };
 }
